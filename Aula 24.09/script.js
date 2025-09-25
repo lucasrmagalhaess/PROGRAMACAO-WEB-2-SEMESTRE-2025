074ad4de2078447581a57c1566a227cdfd2b4d98
LUCAS RODRIGUES DE MAGALHAES <lucas.rmagalhaes1@senacsp.edu.br>
2025-09-24 21:19:55 -0300

    aprendendo e inicializandono javascript

---
+++ Aula 24.09/script.js
@@ -0,0 +1,19 @@
+document.getElementById('btnEnviar').innerText ='Enviar para a NASA';
+
+
+for (let i=0; i<10000; i++)
+
+    {
+        console.log(i);
+       
+        if(i == 0)
+
+        {
+            alert('Faça o Pix')
+        }
+
+        else if(i == 1)
+        {
+            alert('Estou te hackeando')
+        }
+    }
